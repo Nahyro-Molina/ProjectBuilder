e5d90f8e20fbe0d91a4dc0a2868fdd333f3ebb2c
Nahyro Molina <nahyromolina21@gmail.com>
2026-02-07 11:35:46 -0500

    first version of a project building...project

---
+++ TestClass.py
@@ -0,0 +1,9 @@
+"""
+    No clue what this will consist of YET! But here is a brainstormed version of what I have in mind:
+        - Wondering if there should be classes based off the project for example:
+            - Should I have a class for "programming" project? IDK
+        - For now I will have a generic class that will reference the project structure 
+"""
+class TestClass:
+    def __init__(self):
+    
---
+++ test.py
@@ -0,0 +1,27 @@
+import pathlib
+#Objective: Construct a structured environment
+
+
+def main():
+    """ Runner """
+    # Call other functions from here
+    program()
+
+def program():
+    """???"""
+    # We need to create the folders
+    pathlib.Path("libraries").mkdir()
+    pathlib.Path("scripts").mkdir()
+    pathlib.Path("build").mkdir()
+    pathlib.Path("tests").mkdir()
+    pathlib.Path("wiki").mkdir()
+    pathlib.Path("apis").mkdir()
+    pathlib.Path("docs").mkdir()
+    pathlib.Path("bin").mkdir()
+
+    # I have the folders now what?
+
+
+if __name__ == "__main__":
+    main()
+
